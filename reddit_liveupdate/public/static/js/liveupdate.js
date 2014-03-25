ef3a5aaa16c8f3f2ea00aca659c841938c299de0
umbrae <umbrae@reddit.com>
2014-03-25 07:59:19 -0400

    Semicolons ;(

--- reddit_liveupdate/public/static/js/liveupdate.js
+++ reddit_liveupdate/public/static/js/liveupdate.js
@@ -321,7 +321,7 @@ r.liveupdate.EmbedViewer = r.ScrollUpdater.extend({
                     'scrolling': 'no',
                     'frameborder': 0
                 })
-            r.debug("Rendering embed for link: ", $link);
+            r.debug("Rendering embed for link: ", $link)
             $link.replaceWith(iframe)
         }, this)
     },
@@ -336,7 +336,7 @@ r.liveupdate.EmbedViewer = r.ScrollUpdater.extend({
        var data = JSON.parse(ev.data)
        if (data.action === 'dimensionsChange') {
            /* Yuck. A good reason to give embeds unique IDs. */
-           var $embedFrame = $('.id-LiveUpdate_' + data.updateId + ' .embed-' + data.embedIndex);
+           var $embedFrame = $('.id-LiveUpdate_' + data.updateId + ' .embed-' + data.embedIndex)
 
            $embedFrame.attr({
                'width': Math.min(data.width, 480),
@@ -346,7 +346,7 @@ r.liveupdate.EmbedViewer = r.ScrollUpdater.extend({
     },
 
     init: function() {
-        this.start();
+        this.start()
         $(window).on('message', this._handleMessage)
     }
 })
